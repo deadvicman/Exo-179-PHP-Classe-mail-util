45b33784fae67fcf22870d8ab1d9c2af8ad8af42
victorien <deadvicmangame@gmail.com>
2023-02-01 09:52:10 +0100

    exo 178

---
+++ Mail.php
@@ -0,0 +1,21 @@
+<?php
+class Email {
+    private $subject;
+    private $body;
+    private $recipient;
+
+    public function __construct($subject, $body, $recipients) {
+        $this->subject = $subject;
+        $this->body = $body;
+        $this->recipient = $recipients;
+    }
+
+    public function send() {
+        $to = implode(',', $this->recipient);
+        $headers = "From: sender@example.com\r\n";
+        mail($to, $this->subject, $this->body, $headers);
+    }
+}
+
+$email = new Email("Sujet de l'email", "Corps de l'email", ["destinataire1@example.com", "destinataire2@example.com"]);
+$email->send();
